e13702463433023d522f5c52157dfb0fa3d785c4
Unknown6656 <Unknown6656@users.noreply.github.com>
2024-07-28 17:11:07 +0200

    minor fixes/additions

---
+++ Unknown6656.Runtime/__empty.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+#pragma warning disable CA1050 // Declare types in namespaces
+#pragma warning disable IDE1006 // Naming Styles
+
+[StructLayout(LayoutKind.Explicit, CharSet = CharSet.None, Pack = 0, Size = 0), NativeCppClass]
+public struct __empty
+{
+}
