1f0895ece35e0e834a42c84616377f38c35b77ee
Unknown6656 <Unknown6656@users.noreply.github.com>
2025-02-06 21:33:06 +0100

    moved the GDI+ detector from Unknown6656.Console to this repository

--- Unknown6656.Runtime/LibGDIPlusInstaller.cs
+++ Unknown6656.Runtime/LibGDIPlusInstaller.cs
@@ -1,12 +1,93 @@
-﻿namespace Unknown6656.Runtime;
+﻿using System.Runtime.CompilerServices;
+using System.IO;
+using System;
+
+namespace Unknown6656.Runtime;
 
 
 public static class LibGDIPlusInstaller
 {
-    public static bool IsGDIInstalled => false;
+    private static string[] LibraryCandidates = [
+        "libgdiplus.so",
+        "libgdiplus.so.0",
+        "libgdiplus.so.0.0.0",
+        "libgdi+.so",
+        "libgdi+.so.0",
+        "libgdi+.so.0.0.0",
+        "libgdiplus.dll.so",
+        "libgdiplus.dll.so.0",
+        "libgdi+.dll.so",
+        "libgdi+.dll.so.0",
+    ];
+    private const string USR_LIB = "/usr/lib";
 
-    public static void TryInstallGDI()
+
+    public static bool IsGDIInstalled => GDILibraryPath is not null;
+
+    public static FileInfo? GDILibraryPath { get; private set; } = null;
+
+
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    [ModuleInitializer]
+#pragma warning restore CA2255
+    internal static void ResolveLibGDIPlus()
     {
-#warning TODO : Implement LibGDIPlusInstaller.TryInstallGDI
+        static FileInfo? resolve()
+        {
+            if (OS.IsWindows)
+                return new("gdi32.dll");
+
+            FileInfo? fi = null;
+
+            try
+            {
+                foreach (string path in LibraryCandidates)
+                    if ((fi = new(path)).Exists)
+                        return fi;
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                foreach (string path in LibraryCandidates)
+                    if ((fi = new($"{USR_LIB}/{path}")).Exists)
+                        break;
+            }
+            catch
+            {
+            }
+
+            if (fi?.Exists ?? false)
+            {
+                Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", USR_LIB);
+                Environment.SetEnvironmentVariable("MONO_PATH", USR_LIB);
+                Environment.SetEnvironmentVariable("PATH", $"{Environment.GetEnvironmentVariable("PATH")}:{USR_LIB}");
+
+                return fi;
+            }
+
+            // TODO : ?
+
+            return null;
+        }
+
+        if (!IsGDIInstalled)
+            GDILibraryPath = resolve();
+    }
+
+    public static void EnsureGDIPlusInstalled()
+    {
+        if (!IsGDIInstalled)
+            throw new InvalidOperationException("'GDI32.DLL'/'libgdiplus.so' is not installed or could not be found.");
+    }
+
+    public static void InstallGDIPlus()
+    {
+        throw new NotImplementedException();
+
+
+        ResolveLibGDIPlus(); // refresh
     }
 }
